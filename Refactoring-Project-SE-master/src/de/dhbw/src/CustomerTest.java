ba79fdf02dfccf0ecba4ab5994e29f54d1e4fe64
Charon1502 <luca-rutschmann@gmx.de>
2020-05-18 15:49:04 +0200

    Added a Test class for Customer

---
+++ Refactoring-Project-SE-master/src/de/dhbw/src/CustomerTest.java
@@ -0,0 +1,20 @@
+package de.dhbw.src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import de.dhbw.src.Customer;
+
+class CustomerTest {
+
+	@Test
+	void testForStatement() {
+		assertTrue(true);
+	}
+	
+	@Test
+	void testForAmountFor() {
+		assertTrue(true);
+	}
+
+}
