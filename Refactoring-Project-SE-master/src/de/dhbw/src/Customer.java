619c297c5c035db0fefbe64adf115ad1392d4992
Charon1502 <luca-rutschmann@gmx.de>
2020-05-18 23:36:30 +0200

    Added linebreaks into Method createOutput

--- Refactoring-Project-SE-master/src/de/dhbw/src/Customer.java
+++ Refactoring-Project-SE-master/src/de/dhbw/src/Customer.java
@@ -25,6 +25,7 @@ class Customer {
     
     
     public String createOutput() {
+    	
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration<Rental> enum_rentals = rentals.elements();	    
@@ -32,19 +33,25 @@ class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
+        	
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
+            
             //determine amounts for each line
             thisAmount = resolveAmount(each);
+            
             // add frequent renter points
             frequentRenterPoints ++;
+            
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Category.NEW_RELEASE.getPriceCode()) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
+            
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
+        
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
