5dbd8e0f37f631b373602015f475f139027d78ad
Charon1502 <luca-rutschmann@gmx.de>
2020-05-18 16:02:40 +0200

    added and deleted linebreaks

--- Refactoring-Project-SE-master/src/de/dhbw/src/Customer.java
+++ Refactoring-Project-SE-master/src/de/dhbw/src/Customer.java
@@ -4,17 +4,26 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
+	
     private String name;
     private Vector rentals = new Vector();
+
+    
     public Customer (String newname){
         name = newname;
     };
+    
+    
     public void addRental(Rental arg) {
         rentals.addElement(arg);
     };
+    
+
     public String getName (){
         return name;
     };
+    
+    
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
@@ -41,7 +50,8 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
-
+    
+    
     private double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
